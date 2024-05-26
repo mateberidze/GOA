2759b41689757dd98749fc351f764b775c431c65
Vovaadidas777 <mateberidze464@gmail.com>
2024-05-26 17:25:49 +0400

    uptade commit

---
+++ day59/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("colorButton").addEventListener("click", function() {
+        this.style.backgroundColor = "green";
+    });
+});
